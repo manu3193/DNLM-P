feeec1a078da4ab2cea8ba8bbd65725ed878d0b1
manu3193 <manu3193@gmail.com>
2018-02-02 15:45:47 -0600

    Add custom sharpen filter

--- unsharp.cpp
+++ unsharp.cpp
@@ -12,14 +12,21 @@ int main(int, char**)
 
     int            threads = omp_get_max_threads(); // Get threads number
     ipp::IwiSize   tileSize(dstImage.m_size.width, (dstImage.m_size.height + threads - 1)/threads); // One tile per thread
-    IppiBorderSize sobBorderSize = iwiSizeToBorderSize(iwiMaskToSize(ippMskSize3x3)); // Convert mask size to border size
+    IppiSize       kernelSize = iwiMaskToSize(ippMskSize3x3);
+    IppiBorderSize borderSize = iwiSizeToBorderSize(kernelSize); // Convert kernel size to border size
+    
+    const Ipp16s   kernel[3*3] = {-1/8, -1/8, -1/8, -1/8, 16/8, -1/8, -1/8, -1/8, -1/8}; // Define high pass filter
+    int   numberChannels = 1; //Number of channels of the cvt and src tile
+
 
     #pragma omp parallel num_threads(threads)
     {
         // Declare thread-scope variables
-        IppiBorderType border;
-        ipp::IwiImage srcTile, cvtTile, dstTile;
-        Ipp8u *pTileBuffer = NULL;
+        IppiBorderType        border;
+        ipp::IwiImage         srcTile, cvtTile, dstTile;
+        int                   filterBufferSize = 0, filterSpecSize = 0; // Size of the work buffer and filter specification structure required for filtering
+        int                   srcStep = 0, dstStep = 0; //Steps in bytes through src and dst images
+        IppiFilterBorderSpec* pFilterSpec = NULL;   //Filter specification context structure 
 
         // Color convert threading
         #pragma omp for
@@ -40,8 +47,8 @@ int main(int, char**)
         for(IppSizeL row = 0; row < dstImage.m_size.height; row += tileSize.height)
         {
             ipp::IwiRect tile(0, row, tileSize.width, tileSize.height); // Create actual tile rectangle
-            ipp::iwiRoi_CorrectBordersOverlap(sobBorderSize, cvtImage.m_size, &tile); // Check borders overlap and correct tile of necessary
-            border = ipp::iwiRoi_GetTileBorder(ippBorderRepl, sobBorderSize, cvtImage.m_size, tile); // Get actual tile border
+            ipp::iwiRoi_CorrectBordersOverlap(borderSize, cvtImage.m_size, &tile); // Check borders overlap and correct tile of necessary
+            border = ipp::iwiRoi_GetTileBorder(ippBorderRepl, borderSize, cvtImage.m_size, tile); // Get actual tile border
 
             // Get images for current ROI
             cvtTile = cvtImage.GetRoiImage(tile);
@@ -49,8 +56,11 @@ int main(int, char**)
 
             // Run functions
             //ipp::iwiFilterSobel(&cvtTile, &dstTile, iwiDerivHorFirst, ippMskSize3x3, border);
-            ippiFilterSharpenBorderGetBufferSize(tileSize, ippMskSize3x3, ipp8u, ipp16s, 1, pTileBuffer);
-            ippiFilterSharpenBorder_8u_C3R(&cvtTile, 0, &dstTile, 0, ippMskSize3x3, border, pTileBuffer);
+            ippiFilterBorderGetSize(kernelSize, dstTile.m_size, ipp16s, ipp16s, 1, &filterSpecSize, &filterBufferSize);
+            pFilterSpec = (IppiFilterBorderSpec *)ippsMalloc_16s(filterSpecSize);
+            pFilterBuffer = ippsMalloc_16s(filterBufferSize);
+            ippiFilterBorderInit_16s(kernel, kernelSize, 1, ipp8u, 1, numberChannels, ippRndHintAccurate, pFilterSpec);
+            ippiFilterBorder_16s_C1R(&cvtTile, srcStep, &dstTile, dstStep, dstTile.m_size, border, pFilterSpec, pFilterBuffer );
         }
     }
 }
