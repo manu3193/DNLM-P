f32f3e1b92467f8d5b29b998f139e3d8f3ab0a68
manu3193 <manu3193@gmail.com>
2018-03-20 17:24:22 -0600

    add log kernel primitives

--- src/NoAdaptiveUSM.cpp
+++ src/NoAdaptiveUSM.cpp
@@ -13,19 +13,22 @@ int NoAdaptiveUSM::noAdaptativeUSM(const Ipp32f* pSrc, Ipp32f* pDst, float lambd
 
 int NoAdaptiveUSM::generateLoGKernel(int size, double sigma, Ipp32f* pKernel ){
 	
-	Ipp64f min, max, termSum;
+	Ipp32f min, max, termSum;
 	int halfSize  = (size - 1) / 2;
 	int stepSize32f = 0;
-	Ipp64f std2 = (Ipp64f) sigma*sigma;
+	Ipp32f std2 = (Ipp32f) sigma*sigma;
 
 	//Allocate memory for matrix to store (x*x + y*y) term. 
 	Ipp32f* radXY =  ippiMalloc_32f_C1(size, size, &stepSize32f);
+	//Allocate memory for matrix to store gaussian term. 
+	Ipp32f* expRes =  ippiMalloc_32f_C1(size, size, &stepSize32f);
 
 	for (int i = 0; i < size; ++i)
 	{
 		for (int j = 0; j < size; ++j)
 		{
-			radXY[i*stepSize32f + j] = (Ipp64f) ((i - halfSize) * (i - halfSize) + (j - halfSize) * (j - halfSize));
+			radXY[i*stepSize32f + j] = (Ipp32f) ((i - halfSize) * (i - halfSize) + (j - halfSize) * (j - halfSize));
+			expRes[i*stepSize32f + j] = (Ipp32f) ();
 		}
 	}
 
