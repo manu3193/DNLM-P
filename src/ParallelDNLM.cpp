5029d64082e84a46cc7d09b6618e045f97705763
manu3193 <manu3193@gmail.com>
2018-03-21 11:57:50 -0600

    update method NoAdaptiveUSM parameters

--- src/ParallelDNLM.cpp
+++ src/ParallelDNLM.cpp
@@ -47,17 +47,18 @@ Mat ParallelDNLM::processImage(const Mat& inputImage){
     //Set parameters for processing
     int wRSize = 7;
     int wSize_n=3;
-    float sigma_s = wRSize/1.5;
+    double kernelStd = 0.001;
+    int kernelLen = 7;
     float sigma_r = 12; //13
     float lambda = 5.0;
     
-    Mat fDeceivedNLM = filterDNLM(inputImage, wRSize, wSize_n, sigma_r, lambda);
+    Mat fDeceivedNLM = filterDNLM(inputImage, wRSize, wSize_n, sigma_r, lambda, kernelLen, kernelStd);
 
     return fDeceivedNLM;
 }
 
 //Input image must be from 0 to 255
-Mat ParallelDNLM::filterDNLM(const Mat& srcImage, int wSize, int wSize_n, float sigma_r, float lambda){
+Mat ParallelDNLM::filterDNLM(const Mat& srcImage, int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, double kernelStd){
     
     //Status variable helps to check for errors
     IppStatus status = ippStsNoErr;
@@ -94,7 +95,7 @@ Mat ParallelDNLM::filterDNLM(const Mat& srcImage, int wSize, int wSize_n, float 
     //Normalize converted image
     ippiMulC_32f_C1IR(normFactor, pSrc32fImage, stepSize32f, roi);
 
-    //this->nal.noAdaptiveLaplacian(pSrc32fImage, pUSMImage, lambda);
+    this->noAdaptiveUSM.noAdaptiveUSM(pSrc32fImage, pUSMImage, roi, kernelStd, lambda, kernelLen)
     //this->nlmfd.DNLMFilter(pSrc32fImage, pUSMImage, pFilteredImage, wSize, wSize_n, sigma_s, sigma_r);
 
     //putting back everything
