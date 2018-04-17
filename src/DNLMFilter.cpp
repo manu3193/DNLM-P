fce3378508ef7d434c2dc1dd77334582cf34dec0
manu3193 <manu3193@gmail.com>
2018-04-16 23:58:03 -0600

    remove debugging comments/status

--- src/DNLMFilter.cpp
+++ src/DNLMFilter.cpp
@@ -26,7 +26,6 @@ int DNLMFilter::dnlmFilter(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, int
 //Implements dnlm filter for grayscale images.
 int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, const Ipp32f* pUSMImage, int stepBytesUSM, Ipp32f* pDst, int stepBytesDst, IppiSize imageSize, int w, int w_n, float sigma_r){
     //Variable to store status
-    int status, iMin, iMax, jMin, jMax;
     Ipp32f *pEuclDist = NULL;
     int stepBytesEuclDist = 0;
 
@@ -39,20 +38,13 @@ int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, c
     int neighborhoodStartOffset = imageTopLeftOffset - floor(w_n/2);
 
     //Compute the sliding window size
-    IppiSize windowSize = {w, w};
-    IppiSize windowBorderSize = {w + 2*windowTopLeftOffset, w + 2*windowTopLeftOffset};
-    IppiSize neighborhoodSize = {w_n, w_n};
-
+    const IppiSize windowSize = {w, w};
+    const IppiSize windowBorderSize = {w + 2*windowTopLeftOffset, w + 2*windowTopLeftOffset};
+    const IppiSize neighborhoodSize = {w_n, w_n};
 
     //Allocate memory for sqrtDist matrix
     pEuclDist = ippiMalloc_32f_C1(windowSize.width, windowSize.height, &stepBytesEuclDist);
 
-    Ipp32f *pWindowStart, *pNeighborhoodStartIJ, *pNeighborhoodStartNM, *pUSMWindowStart;
-
-    
-    cout << "Window H: "<< windowSize.height << " W: " << windowSize.width <<endl;
-    cout << "Window w/border H: "<< windowBorderSize.height << " W: " << windowBorderSize.width <<endl;
-    cout << "Neighborhood H: "<< neighborhoodSize.height << " W: " << neighborhoodSize.width <<endl;
 
     for (int j = 0; j < imageSize.height; ++j)
     {
@@ -64,28 +56,9 @@ int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, c
         for (int i = 0; i < imageSize.width; ++i)
         {
             
-            pWindowStart = (Ipp32f *) &pSrcBorder[indexWindowStartBase+i]; 
-            pNeighborhoodStartIJ = (Ipp32f *) &pSrcBorder[indexNeighborIJBase + (i + neighborhoodStartOffset)];
-            pUSMWindowStart = (Ipp32f *) &pUSMImage[indexUSMWindowBase+(i + windowTopLeftOffset)];
-
-            // cout << "window : "<<endl;
-            // for (int r = 0; r < windowBorderSize.height; ++r)
-            // {
-            //     for (int s = 0; s < windowBorderSize.width; ++s)
-            //     {
-            //         cout << pWindowStart[r*(stepBytesSrcBorder/sizeof(Ipp32f)) + s] << " ";
-            //     }
-            //     cout <<endl;
-            // }
-            // cout << "IJ : "<<endl;
-            // for (int r = 0; r < neighborhoodSize.height; ++r)
-            // {
-            //     for (int s = 0; s < neighborhoodSize.width; ++s)
-            //     {
-            //         cout << pNeighborhoodStartIJ[r*(stepBytesSrcBorder/sizeof(Ipp32f)) + s] << " ";
-            //     }
-            //     cout <<endl;
-            // }
+            const Ipp32f *pWindowStart = (Ipp32f *) &pSrcBorder[indexWindowStartBase+i]; 
+            const Ipp32f *pNeighborhoodStartIJ = (Ipp32f *) &pSrcBorder[indexNeighborIJBase + (i + neighborhoodStartOffset)];
+            const Ipp32f *pUSMWindowStart = (Ipp32f *) &pUSMImage[indexUSMWindowBase+(i + windowTopLeftOffset)];
 
             for (int n = 0; n < windowSize.height; ++n)
             {
@@ -94,43 +67,21 @@ int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, c
 
                 for (int m = 0; m < windowSize.width; ++m)
                 {
-                    pNeighborhoodStartNM = &pWindowStart[indexNeighborNMBase + m];
-
-                     /*cout << "NM : " <<endl;
-                     for (int r = 0; r < neighborhoodSize.height; ++r)
-                     {
-                         for (int s = 0; s < neighborhoodSize.width; ++s)
-                         {
-                             cout << pNeighborhoodStartNM[r*(stepBytesSrcBorder/sizeof(Ipp32f)) + s] << " ";
-                         }
-                         cout <<endl;
-                     }*/
-
-
-                    status = ippiNormDiff_L2_32f_C1R(pNeighborhoodStartNM, stepBytesSrcBorder, pNeighborhoodStartIJ, stepBytesSrcBorder, neighborhoodSize, &euclDistResult, ippAlgHintNone);
+                    const pNeighborhoodStartNM = &pWindowStart[indexNeighborNMBase + m];
+                    ippiNormDiff_L2_32f_C1R(pNeighborhoodStartNM, stepBytesSrcBorder, pNeighborhoodStartIJ, stepBytesSrcBorder, neighborhoodSize, &euclDistResult, ippAlgHintNone);
                     pEuclDist[indexEuclDistBase + m] = (Ipp32f) euclDistResult;
                 }
             }
-
-            // cout << "euclDistResult : "<<endl;
-            // for (int r = 0; r < windowSize.height; ++r)
-            // {
-            //     for (int s = 0; s < windowSize.width; ++s)
-            //     {
-            //         cout << pEuclDist[r*(stepBytesEuclDist/sizeof(Ipp32f)) + s] << " ";
-            //     }
-            //     cout <<endl;
-            // }
             
-            status = ippiDivC_32f_C1IR((Ipp32f) -(sigma_r * sigma_r), pEuclDist, stepBytesEuclDist, windowSize);
-            status = ippiExp_32f_C1IR(pEuclDist, stepBytesEuclDist, windowSize);
-            status = ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &sumExpTerm, ippAlgHintNone);
-            status = ippiMul_32f_C1IR(pUSMWindowStart, stepBytesUSM, pEuclDist, stepBytesEuclDist, windowSize);
-            status = ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &filterResult, ippAlgHintNone);
+            ippiDivC_32f_C1IR((Ipp32f) -(sigma_r * sigma_r), pEuclDist, stepBytesEuclDist, windowSize);
+            ippiExp_32f_C1IR(pEuclDist, stepBytesEuclDist, windowSize);
+            ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &sumExpTerm, ippAlgHintNone);
+            ippiMul_32f_C1IR(pUSMWindowStart, stepBytesUSM, pEuclDist, stepBytesEuclDist, windowSize);
+            ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &filterResult, ippAlgHintNone);
 
             pDst[indexPdstBase+i] = (Ipp32f) (filterResult/ sumExpTerm);
-            //cout << pDst[j*(stepBytesDst/sizeof(Ipp32f))+i] << " ";
 
+            //Need to do better error handling 
             if(status!=ippStsNoErr) cout << "Error " << status << endl;
 
         }
