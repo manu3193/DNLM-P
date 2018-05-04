c8bd764109958f50040e1fdf8fab781da6a15998
manu3193 <manu3193@gmail.com>
2018-05-03 23:43:40 -0600

    add parallel pragma

--- src/DNLMFilter.cpp
+++ src/DNLMFilter.cpp
@@ -25,13 +25,7 @@ int DNLMFilter::dnlmFilter(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, int
 
 //Implements dnlm filter for grayscale images.
 int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, const Ipp32f* pUSMImage, int stepBytesUSM, Ipp32f* pDst, int stepBytesDst, IppiSize imageSize, int w, int w_n, float sigma_r){
-    //Variable to store status
-    Ipp32f *pEuclDist = NULL;
-    int stepBytesEuclDist = 0;
-
-    //Variable to store summation result of filter response dor normalization
-    Ipp64f sumExpTerm = 0, filterResult = 0, euclDistResult = 0;
-
+    
     //Compute border offset for border replicated image
     int windowTopLeftOffset = floor(w_n/2);
     int imageTopLeftOffset = floor(w/2) + windowTopLeftOffset;
@@ -42,53 +36,60 @@ int DNLMFilter::dnlmFilterBW(const Ipp32f* pSrcBorder, int stepBytesSrcBorder, c
     const IppiSize windowBorderSize = {w + 2*windowTopLeftOffset, w + 2*windowTopLeftOffset};
     const IppiSize neighborhoodSize = {w_n, w_n};
 
-    //Allocate memory for sqrtDist matrix
-    pEuclDist = ippiMalloc_32f_C1(windowSize.width, windowSize.height, &stepBytesEuclDist);
-
-
-    for (int j = 0; j < imageSize.height; ++j)
+    #pragma omp parallel shared(pDst,pUSMImage,pSrcBorder) 
     {
-        const int indexPdstBase = j*(stepBytesDst/sizeof(Ipp32f));
-        const int indexWindowStartBase = j*(stepBytesSrcBorder/sizeof(Ipp32f));
-        const int indexNeighborIJBase = (j + neighborhoodStartOffset)*(stepBytesSrcBorder/sizeof(Ipp32f));
-        const int indexUSMWindowBase =(j + windowTopLeftOffset)*(stepBytesUSM/sizeof(Ipp32f));
+        //Variable to store status
+        Ipp32f *pEuclDist = NULL;
+        int stepBytesEuclDist = 0;
+         //Variable to store summation result of filter response dor normalization
+        Ipp64f sumExpTerm = 0, filterResult = 0, euclDistResult = 0;
 
-        for (int i = 0; i < imageSize.width; ++i)
+        //Allocate memory for sqrtDist matrix
+        pEuclDist = ippiMalloc_32f_C1(windowSize.width, windowSize.height, &stepBytesEuclDist);
+
+        #pragma omp for collapse(2)
+        for (int j = 0; j < imageSize.height; ++j)
         {
-            
-            const Ipp32f *pWindowStart = &pSrcBorder[indexWindowStartBase+i]; 
-            const Ipp32f *pNeighborhoodStartIJ = &pSrcBorder[indexNeighborIJBase + (i + neighborhoodStartOffset)];
-            const Ipp32f *pUSMWindowStart = &pUSMImage[indexUSMWindowBase+(i + windowTopLeftOffset)];
-
-            for (int n = 0; n < windowSize.height; ++n)
+            for (int i = 0; i < imageSize.width; ++i)
             {
-                const int indexEuclDistBase = n * (stepBytesEuclDist/sizeof(Ipp32f));
-                const int indexNeighborNMBase = n * (stepBytesSrcBorder/sizeof(Ipp32f));
+                const int indexPdstBase = j*(stepBytesDst/sizeof(Ipp32f));
+                const int indexWindowStartBase = j*(stepBytesSrcBorder/sizeof(Ipp32f));
+                const int indexNeighborIJBase = (j + neighborhoodStartOffset)*(stepBytesSrcBorder/sizeof(Ipp32f));
+                const int indexUSMWindowBase =(j + windowTopLeftOffset)*(stepBytesUSM/sizeof(Ipp32f));
+                const Ipp32f *pWindowStart = &pSrcBorder[indexWindowStartBase+i]; 
+                const Ipp32f *pNeighborhoodStartIJ = &pSrcBorder[indexNeighborIJBase + (i + neighborhoodStartOffset)];
+                const Ipp32f *pUSMWindowStart = &pUSMImage[indexUSMWindowBase+(i + windowTopLeftOffset)];
 
-                for (int m = 0; m < windowSize.width; ++m)
+                for (int n = 0; n < windowSize.height; ++n)
                 {
-                    const Ipp32f *pNeighborhoodStartNM = &pWindowStart[indexNeighborNMBase + m];
-                    ippiNormDiff_L2_32f_C1R(pNeighborhoodStartNM, stepBytesSrcBorder, pNeighborhoodStartIJ, stepBytesSrcBorder, neighborhoodSize, &euclDistResult, ippAlgHintNone);
-                    pEuclDist[indexEuclDistBase + m] = (Ipp32f) euclDistResult;
+                    const int indexEuclDistBase = n * (stepBytesEuclDist/sizeof(Ipp32f));
+                    const int indexNeighborNMBase = n * (stepBytesSrcBorder/sizeof(Ipp32f));
+
+                    for (int m = 0; m < windowSize.width; ++m)
+                    {
+                        const Ipp32f *pNeighborhoodStartNM = &pWindowStart[indexNeighborNMBase + m];
+                        ippiNormDiff_L2_32f_C1R(pNeighborhoodStartNM, stepBytesSrcBorder, pNeighborhoodStartIJ, stepBytesSrcBorder, neighborhoodSize, &euclDistResult, ippAlgHintNone);
+                        pEuclDist[indexEuclDistBase + m] = (Ipp32f) euclDistResult;
+                    }
                 }
+                
+                ippiDivC_32f_C1IR((Ipp32f) -(sigma_r * sigma_r), pEuclDist, stepBytesEuclDist, windowSize);
+                ippiExp_32f_C1IR(pEuclDist, stepBytesEuclDist, windowSize);
+                ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &sumExpTerm, ippAlgHintNone);
+                ippiMul_32f_C1IR(pUSMWindowStart, stepBytesUSM, pEuclDist, stepBytesEuclDist, windowSize);
+                ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &filterResult, ippAlgHintNone);
+
+                pDst[indexPdstBase+i] = (Ipp32f) (filterResult/ sumExpTerm);
+
+                //Need to do better error handling 
+                //if(status!=ippStsNoErr) cout << "Error " << status << endl;
+
             }
-            
-            ippiDivC_32f_C1IR((Ipp32f) -(sigma_r * sigma_r), pEuclDist, stepBytesEuclDist, windowSize);
-            ippiExp_32f_C1IR(pEuclDist, stepBytesEuclDist, windowSize);
-            ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &sumExpTerm, ippAlgHintNone);
-            ippiMul_32f_C1IR(pUSMWindowStart, stepBytesUSM, pEuclDist, stepBytesEuclDist, windowSize);
-            ippiSum_32f_C1R(pEuclDist, stepBytesEuclDist, windowSize, &filterResult, ippAlgHintNone);
-
-            pDst[indexPdstBase+i] = (Ipp32f) (filterResult/ sumExpTerm);
-
-            //Need to do better error handling 
-            //if(status!=ippStsNoErr) cout << "Error " << status << endl;
-
         }
+        ippiFree(pEuclDist);
     }
 
-
-    ippiFree(pEuclDist);
+    
 
     return 1;
     
