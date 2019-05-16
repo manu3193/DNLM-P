af143bcda6a8d6301aa4ca5441e569d13391466c
Manuel Zumbado <manu3193@gmail.com>
2019-05-16 13:59:28 -0600

    add openacc code

--- include/ParallelDNLM.hpp
+++ include/ParallelDNLM.hpp
@@ -1,25 +1,82 @@
-
 #ifndef PARALLELDNLM_HPP_
 #define PARALLELDNLM_HPP_
 
-#include <iostream>
 #include <string>
-#include <ippcv.h>
-#include <DNLMFilter.hpp>
-#include <NoAdaptiveUSM.hpp>
-#include <timer.h>
+#include <iostream>
+#include <regex>
+//#include <DNLMFilter.hpp>
+//#include <NoAdaptiveUSM.hpp>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <npp.h>
+
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
+
+
+/**
+ * @brief      Class for parallel DNLM filter.
+ */
 class ParallelDNLM{
 	
 public:
-	Mat processImage(const Mat& U);
+	
+	/**
+	 * @brief      Constructs the object with default parameters
+	 */
+	ParallelDNLM();
 
+	/**
+	 * @brief      Constructs the object with the given parameters
+	 *
+	 * @param[in]  wSize      The window size S = wSize x wSize
+	 * @param[in]  wSize_n    The neighborhood size W = wSize_n x wSize_n
+	 * @param[in]  sigma_r    The filter bandwidth h = 2 * sigma_r
+	 * @param[in]  lambda     The lambda gain of USM filter
+	 * @param[in]  kernelLen  The kernel length of USM filter USM_kernel_len = kernelLen * kernelLen
+	 * @param[in]  kernelStd  The kernel std of USM filter. 
+	 * 
+	 * For recomended parameters use 6 * kernelStd = kernelLen + 2
+	 * 
+	 */
+        ParallelDNLM(int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, float kernelStd);
+	
+	/**
+	 * @brief      Process the image
+	 *
+	 * @param[in]  U     Input image
+	 *
+	 * @return     Processed image as opencv Mat object
+	 */
+	Mat processImage(const Mat);
 private:
-	DNLMFilter dnlmFilter;
-	NoAdaptiveUSM noAdaptiveUSM;
-	Mat filterDNLM(const Mat& U, int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, double kernelStd);
+
+//	DNLMFilter dnlmFilter;
+//	NoAdaptiveUSM noAdaptiveUSM;
+
+	int wSize;
+        int wSize_n;
+        float kernelStd;
+        int kernelLen;
+        float sigma_r; 
+        float lambda;
+
+	/**
+	 * @brief      Filter the image with the given parameters
+	 *
+	 * @param[in]  U          Input image
+	 * @param[in]  wSize      The window size S = wSize x wSize
+	 * @param[in]  wSize_n    The neighborhood size W = wSize_n x wSize_n
+	 * @param[in]  sigma_r    The filter bandwidth h = 2 * sigma_r
+	 * @param[in]  lambda     The lambda gain of USM filter
+	 * @param[in]  kernelLen  The kernel length of USM filter USM_kernel_len = kernelLen * kernelLen
+	 * @param[in]  kernelStd  The kernel std of USM filter. 
+	 *
+	 * @return int status     
+	 */
+         Mat filterDNLM(const Mat, int , int , float , float , int , float);
 };
 #endif /* PARALLELDNLM_HPP_ */
--- src/ParallelDNLM.cpp
+++ src/ParallelDNLM.cpp
@@ -1,155 +1,204 @@
-
 #include <ParallelDNLM.hpp>
 
 using namespace std;
   
+void DNLM_OpenACC(const float*, int, const float*, int, float*, int, int, int, int, int, int, int, int, int, float);
 
+/**
+ *  * @brief      Constructs the object with default parameters
+ *   */
+ParallelDNLM::ParallelDNLM(){
+    this->wSize = 21;
+    this->wSize_n = 7;
+    this->kernelStd = 3;
+    this->kernelLen = 16;
+    this->sigma_r = 1; 
+    this->lambda = 1;
+}
+
+/**
+ *  * @brief      Constructs the object with the given parameters
+ *   *
+ *    * @param[in]  wSize      The window size S = wSize x wSize
+ *     * @param[in]  wSize_n    The neighborhood size W = wSize_n x wSize_n
+ *      * @param[in]  sigma_r    The filter bandwidth h = 2 * sigma_r
+ *       * @param[in]  lambda     The lambda gain of USM filter
+ *        * @param[in]  kernelLen  The kernel length of USM filter USM_kernel_len = kernelLen * kernelLen
+ *         * @param[in]  kernelStd  The kernel std of USM filter. 
+ *          * 
+ *           * For recomended parameters use 6 * kernelStd = kernelLen + 2
+ *            * 
+ *             */
+ParallelDNLM::ParallelDNLM(int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, float kernelStd){
+    this->wSize = wSize;
+    this->wSize_n = wSize_n;
+    this->kernelStd = kernelStd;
+    this->kernelLen = kernelLen;
+    this->sigma_r = sigma_r; 
+    this->lambda = lambda;
+}
 
 int main(int argc, char* argv[]){
-    ParallelDNLM parallelDNLM;
+    ParallelDNLM *parallelDNLM;
 
+    //regex intRegex = regex("[+]?[0-9]+");
+    //regex floatRegex = regex("[+]?([0-9]*[.])?[0-9]+");
+
+    if (argc == 2){
+        cout << "Using default parameters W=21x21, W_n=7x7, sigma_r=10, lambda=1, USM_len=16,USM_std=3" << endl;
+        parallelDNLM = new ParallelDNLM();
+    } 
     //Check input arguments
-    if (argc != 2){
-        cout << "ERROR: You must provide a valid image filename" << endl;
+    else if (argc !=8){
+        cerr << "Error parsing parameters, please look at the documentation for correct use" <<endl;
         return -1;
-    }else{
+    }
+    //else if (!regex_match(string(argv[2]), intRegex) & !regex_match(string(argv[3]), intRegex) & !regex_match(string(argv[6]), intRegex)){
+    //    return -1;
+    //}
+    //else if (!regex_match(string(argv[4]), floatRegex) & !regex_match(string(argv[5]), floatRegex)  & !regex_match(string(argv[7]), floatRegex)){
+    //    return -1;
+    //}
+    else { 
+        parallelDNLM = new ParallelDNLM(stoi(string(argv[2])), stoi(string(argv[3])), stof(string(argv[4])), stof(string(argv[5])), stoi(string(argv[6])), stof(string(argv[7])));
+    } 
 
     //Open input image
     const string inputFile = argv[1];
     // Find extension point
     string::size_type pAt = inputFile.find_last_of('.');
-
     // Form the new name with container
     const string outputFile = inputFile.substr(0, pAt) + "_DeNLM.png";
-
-    Mat inputImage, outputImage;
-
     //This version only works with grayscale images
-    inputImage = imread(inputFile, IMREAD_GRAYSCALE);
+    Mat inputImage = imread(inputFile, IMREAD_GRAYSCALE);
     //Check for errors when loading image
-        if(!inputImage.data){
-            cout << "Could not read image from file." << endl;
-            return -1;
-        }
-    
-    //Init IPP library
-    IppStatus status = ippStsNoErr;
-    status = ippInit();
-    if(status != ippStsNoErr)
-        cerr <<"Processor not identified"<<endl;
-    
-    outputImage = parallelDNLM.processImage(inputImage);
-
+    if(!inputImage.data){
+        cout << "Could not read image from file." << endl;
+        return -1;
+    }
+    //Process image 
+    Mat outputImage  = parallelDNLM->processImage(inputImage);
     //Write image to output file.
     imwrite(outputFile, outputImage);
-
     //Release object memory
     inputImage.release();
     outputImage.release();
-    
+
     return 0;
-    }
-}
+                                                                                                                                                                                              }
 
 
-
-Mat ParallelDNLM::processImage(const Mat& inputImage){
-    //Set parameters for processing
-    int wRSize = 21;
-    int wSize_n=7;
-    float kernelStd = 0.001f;
-    int kernelLen = 17;
-    float sigma_r = 0.5f; //13
-    float lambda = 3.0f;
-    
-    Mat fDeceivedNLM = filterDNLM(inputImage, wRSize, wSize_n, sigma_r, lambda, kernelLen, kernelStd);
+Mat ParallelDNLM::processImage(const Mat inputImage){
+   //Set Parameters for processing 
+    Mat fDeceivedNLM = filterDNLM(inputImage, wSize, wSize_n, sigma_r, lambda, kernelLen, kernelStd);
 
     return fDeceivedNLM;
 }
 
 //Input image must be from 0 to 255
-Mat ParallelDNLM::filterDNLM(const Mat& srcImage, int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, double kernelStd){
+Mat ParallelDNLM::filterDNLM(const Mat inputImage, int wSize, int wSize_n, float sigma_r, float lambda, int kernelLen, float kernelStd){
     
+    //Get cuda device properties
+    //int device;
+    cudaError_t cudaStatus = cudaSuccess;
+    //Create CUDA events to measure execution time
+    cudaEvent_t start, stop;
+    //cudaEventCreate(&start);
+    //cudaEventCreate(&stop);
     //Status variable helps to check for errors
-    IppStatus status = ippStsNoErr;
-
-    //Pointers to IPP type images 
-    Ipp32f *pSrc32fImage __attribute__((aligned(64)));
-    Ipp32f *pSrcwBorderImage __attribute__((aligned(64)));
-    Ipp32f *pSqrBorderImage __attribute__((aligned(64)));
-    Ipp32f *pSqrIntegralImage __attribute__((aligned(64)));
-    Ipp32f *pUSMImage __attribute__((aligned(64)));
-    Ipp32f *pFilteredImage __attribute__((aligned(64)));
-
+    NppStatus status =  NPP_NO_ERROR;
+    //Pointers to NPP type images 
+    Npp32f *pIntegralImage32f = NULL, *pSrcwBorderImage32f = NULL, *pFilteredImage32f = NULL;
+    Npp64f *pSqrIntegralImage64f = NULL;
+    Npp8u  *pSrcImage8u = NULL, *pSrcwBorderImage8u = NULL, *pFilteredImage8u = NULL;
     //Variable to store image step size in bytes 
-    int stepBytesSrc = 0;
-    int stepBytesSrcwBorder = 0;
-    int stepBytesSrcSqr = 0;
-    int stepBytesSqrIntegral = 0;
-    int stepBytesUSM = 0;
-    int stepBytesFiltered = 0;
-
+    int stepBytesSrcwBorder8u = 0;
+    int stepBytesSrcwBorder32f = 0;
+    int stepBytesSrc8u = 0;
+    int stepBytesSqrIntegral64f = 0;
+    int stepBytesFiltered32f = 0;
+    int stepBytesFiltered8u = 0;
+    int stepBytesIntegral = 0;
     //Scale factors to normalize and denormalize 32f image
-    Ipp32f normFactor = 1.0/255.0; 
-    Ipp32f scaleFactor = 255.0; 
+    Npp64f normFactor64f = 1.0/255.0;
+    Npp32f normFactor32f = 1.0/255.0; 
+    Npp32f scaleFactor = 255.0; 
 
     //cv::Mat output filtered image
-    Mat outputImage = Mat(srcImage.size(), srcImage.type());
+    Mat outputImage = Mat(inputImage.size(), inputImage.type());
 
     //Variables used for image format conversion
-    IppiSize imageROISize, imageROIwBorderSize, imageROIwBorderIISize;
-    imageROISize.width = srcImage.size().width;
-    imageROISize.height = srcImage.size().height;
-
-    //Get pointer to src and dst data
-    Ipp8u *pSrcImage = (Ipp8u*)&srcImage.data[0];                               
-    Ipp8u *pDstImage = (Ipp8u*)&outputImage.data[0];   
+    NppiSize imageROISize, imageROIwBorderSize, integralImageROISize;
+    imageROISize.width = inputImage.size().width;
+    imageROISize.height = inputImage.size().height;
 
     //Compute border offset for border replicated image
-    const int windowTopLeftOffset = floor(wSize_n/2);
-    const int imageTopLeftOffset = floor(wSize/2) + windowTopLeftOffset;
+    const int windowRadius = floor(wSize/2);
+    const int neighborRadius = floor(wSize_n/2);
+    const int imageTopLeftOffset = windowRadius + neighborRadius;
 
     imageROIwBorderSize = {imageROISize.width + 2*imageTopLeftOffset, imageROISize.height + 2*imageTopLeftOffset};     
-    imageROIwBorderIISize = {imageROIwBorderSize.width + 1, imageROIwBorderSize.height + 1}; 
+    integralImageROISize = {imageROIwBorderSize.width+1, imageROIwBorderSize.height+1};
+    //Allocate memory for gpu images
+    pSrcImage8u = nppiMalloc_8u_C1(imageROISize.width, imageROISize.height, &stepBytesSrc8u);
+    pSrcwBorderImage32f = nppiMalloc_32f_C1(imageROIwBorderSize.width, imageROIwBorderSize.height, &stepBytesSrcwBorder32f); 
+    pIntegralImage32f = nppiMalloc_32f_C1(integralImageROISize.width, integralImageROISize.height, &stepBytesIntegral);
+    pSrcwBorderImage8u = nppiMalloc_8u_C1(imageROIwBorderSize.width, imageROIwBorderSize.height, &stepBytesSrcwBorder8u);
+    pFilteredImage32f = nppiMalloc_32f_C1(imageROISize.width, imageROISize.height, &stepBytesFiltered32f);   
+    pFilteredImage8u = nppiMalloc_8u_C1(imageROISize.width, imageROISize.height, &stepBytesFiltered8u);
     
-    //Allocate memory for images
-    pSrc32fImage = ippiMalloc_32f_C1(imageROISize.width, imageROISize.height, &stepBytesSrc); 
-    pSrcwBorderImage = ippiMalloc_32f_C1(imageROIwBorderSize.width, imageROIwBorderSize.height, &stepBytesSrcwBorder);
-    pSqrBorderImage = ippiMalloc_32f_C1(imageROIwBorderSize.width, imageROIwBorderSize.height, &stepBytesSrcSqr);
-    pSqrIntegralImage = ippiMalloc_32f_C1(imageROIwBorderIISize.width, imageROIwBorderIISize.height, &stepBytesSqrIntegral);
-    pUSMImage = ippiMalloc_32f_C1(imageROIwBorderSize.width, imageROIwBorderSize.height, &stepBytesUSM);
-    pFilteredImage = ippiMalloc_32f_C1(imageROISize.width, imageROISize.height, &stepBytesFiltered);   
-    
-    //Convert input image to 32f format
-    ippiConvert_8u32f_C1R(pSrcImage, srcImage.step[0], pSrc32fImage, stepBytesSrc, imageROISize);
-    //Normalize converted image
-    ippiMulC_32f_C1IR(normFactor, pSrc32fImage, stepBytesSrc, imageROISize);
-
+    cudaMemcpy2D(pSrcImage8u, stepBytesSrc8u, &inputImage.data[0], inputImage.step[0], imageROISize.width, imageROISize.height, cudaMemcpyHostToDevice);
+ 
+    cudaStatus = cudaMallocPitch(&pSqrIntegralImage64f, (size_t*) &stepBytesSqrIntegral64f, integralImageROISize.width * sizeof(Npp64f), integralImageROISize.height);
+    //Set result gpu image to 0
+    //nppiSet_32f_C1R((Npp32f) 0.0f, pFilteredImage32f, stepBytesFiltered32f, {imageROISize.width, imageROISize.height});
     // Mirror border for full image filtering
-    status = ippiCopyMirrorBorder_32f_C1R(pSrc32fImage, stepBytesSrc, imageROISize, pSrcwBorderImage, stepBytesSrcwBorder, imageROIwBorderSize, imageTopLeftOffset, imageTopLeftOffset);
-    //Compute squared image
-    status = ippiSqr_32f_C1R(pSrcwBorderImage, stepBytesSrcwBorder, pSqrBorderImage, stepBytesSrcSqr, imageROIwBorderSize);
-    //Compute Squared Integral Image
-    status = ippiIntegral_32f_C1R(pSqrBorderImage, stepBytesSrcwBorder, pSqrIntegralImage, stepBytesSqrIntegral, imageROIwBorderSize);
+    status = nppiCopyWrapBorder_8u_C1R(pSrcImage8u, stepBytesSrc8u, imageROISize, pSrcwBorderImage8u, stepBytesSrcwBorder8u, imageROIwBorderSize, imageTopLeftOffset, imageTopLeftOffset);
+    // Compute Sqr Integral Image
+    status = nppiSqrIntegral_8u32f64f_C1R(pSrcwBorderImage8u, stepBytesSrcwBorder8u, pIntegralImage32f, stepBytesIntegral, pSqrIntegralImage64f, stepBytesSqrIntegral64f, imageROIwBorderSize, 0, 0);
+    //Convert input image to 32f format
+    status = nppiConvert_8u32f_C1R(pSrcwBorderImage8u, stepBytesSrcwBorder8u, pSrcwBorderImage32f, stepBytesSrcwBorder32f, imageROIwBorderSize);
+    // Normalize input
+    status = nppiMulC_32f_C1IR(normFactor32f, pSrcwBorderImage32f, stepBytesSrcwBorder32f, imageROIwBorderSize);
+    status = nppsMulC_64f_I(normFactor64f, pSqrIntegralImage64f, integralImageROISize.height*stepBytesSqrIntegral64f/sizeof(Npp64f));   
+    status = nppsConvert_64f32f(pSqrIntegralImage64f, pIntegralImage32f, integralImageROISize.height*stepBytesSqrIntegral64f/sizeof(Npp64f));
+    //Normalize converted image
+    //ippiMulC_32f_C1IR(normFactor, pSrc32fImage, stepBytesSrc, imageROISize);
+
     //timer start
-    timerStart();
-    
-    this->noAdaptiveUSM.noAdaptiveUSM(pSrcwBorderImage, stepBytesSrcwBorder, pUSMImage, stepBytesUSM, imageROIwBorderSize, kernelStd, lambda, kernelLen);
-    this->dnlmFilter.dnlmFilter(pSrcwBorderImage, stepBytesSrcwBorder, CV_32FC1, pUSMImage, stepBytesUSM, pSqrIntegralImage, stepBytesSqrIntegral, pFilteredImage, stepBytesFiltered, imageROISize, wSize, wSize_n, sigma_r);
+    //cudaEventRecord(start); 
+    //Applying USM Filter
+    //OLD status =nppiFilterUnsharpBorder_32f_C1R(pSrcwBorderImage, stepBytesSrcwBorder, {imageTopLeftOffset,imageTopLeftOffset}, pUSMImage, stepBytesUSM, {imageROIwBorderSize.width, imageROIwBorderSize.height}, usmRadius, kernelStd, lambda, 2, NPP_BORDER_REPLICATE, pUSMBuffer);
+    //Gossens version doesnt works with normalized images
+    //ippiMulC_32f_C1IR(scaleFactor, pSrcwBorderImage, stepBytesSrcwBorder, imageROIwBorderSize);
+    //ippiMulC_32f_C1IR(scaleFactor, pUSMImage, stepBytesUSM, imageROIwBorderSize);
+    //Aplying DNLM filter
+    //OLD this->dnlmFilter.dnlmFilter(pSrcwBorderImage, stepBytesSrcwBorder, CV_32FC1, pUSMImage, stepBytesUSM, pFilteredImage32f, stepBytesFiltered32f,  {imageROIwBorderSize.width, imageROIwBorderSize.height}, wSize, wSize_n, sigma_r);
+    DNLM_OpenACC(pSrcwBorderImage32f, stepBytesSrcwBorder32f, pIntegralImage32f, stepBytesIntegral, pFilteredImage32f, stepBytesFiltered32f, windowRadius, neighborRadius, imageROISize.width, imageROISize.height, wSize , wSize, wSize_n, wSize_n, sigma_r);
 
-    double time = timerStop();
+    //Measure slapsed time
+    //cudaEventRecord(stop);
+    //Unormalize
+    status = nppiMulC_32f_C1IR(scaleFactor, pFilteredImage32f, stepBytesFiltered32f, imageROISize);
+    //Convert back to uchar, add offset to pointer to remove border
+    nppiConvert_32f8u_C1R(pFilteredImage32f, stepBytesFiltered32f, pFilteredImage8u, stepBytesFiltered8u, imageROISize, NPP_RND_FINANCIAL);
 
-    //putting back everything
-    ippiMulC_32f_C1IR(scaleFactor, pFilteredImage, stepBytesFiltered, imageROISize);
-    ippiConvert_32f8u_C1R(pFilteredImage, stepBytesFiltered, pDstImage , outputImage.step[0], imageROISize, ippRndFinancial);
+    cudaMemcpy2D(&outputImage.data[0], outputImage.step[0], pFilteredImage8u ,stepBytesFiltered8u, imageROISize.width, imageROISize.height, cudaMemcpyDeviceToHost);
     
+    //cudaEventSynchronize(stop);
+    //float elapsed = 0;
+   // cudaEventElapsedTime(&elapsed, start, stop);
     //Freeing memory
-    ippiFree(pSrc32fImage);
-    ippiFree(pSrcwBorderImage);
-    ippiFree(pUSMImage);
-    ippiFree(pFilteredImage);
-
-    cout << time << endl;
+    nppiFree(pSrcImage8u); 
+    nppiFree(pSrcwBorderImage8u);
+    nppiFree(pSrcwBorderImage32f);
+    nppiFree(pFilteredImage32f);
+    nppiFree(pIntegralImage32f);
+    nppsFree(pSqrIntegralImage64f);
+    //nppiFree(pUSMImage);
+    nppiFree(pFilteredImage8u);    
+    //nppsFree(pUSMBuffer);
+    //cout << elapsed/1000 <<endl;
 
     return outputImage;
 }
