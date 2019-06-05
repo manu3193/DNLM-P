200be6a3c53fb70e7ac620f8cbb699c2f8a45cd0
manu3193 <manu3193@gmail.com>
2019-06-05 11:52:21 -0600

    fix bug

--- src/DNLMFilter_OpenACC.cpp
+++ src/DNLMFilter_OpenACC.cpp
@@ -7,15 +7,15 @@ void DNLM_OpenACC(const float* pSrcBorder, int stepBytesSrcBorder, const float* 
 
     //Array to store window matrix for euclidean distance
     float * restrict pEuclDist = (float*) malloc(windowHeight * windowWidth * sizeof(float));
-    float * restrict pNeighborhoodDiff = (float*) malloc(neighborHeight * neighborWidth * sizeof(float)); 
+    float * restrict pWindowIJCorr = (float*) malloc(windowHeight * windowWidth * sizeof(float)); 
     //Region de datos privada peucldist (create)
     //Nivel de paralelismo gangs collapse
-    #pragma acc data deviceptr(pSrcBorder[(windowHeight+2*(windowRadius+neighborRadius))*(windowWidth+2*(windowRadius+neighborRadius))], pDst[imageHeight*imageWidth]) create(pEuclDist[0:windowHeight*windowWidth], pNeighborhoodDiff[0:neighborHeight*neighborWidth]) 
-    //#pragma acc data present(pSrcBorder[0:(imageWidth+windowRadius+neighborRadius)+stepBytesSrcBorder/sizeof(float)*(imageHeight+windowRadius+neighborRadius)], pDst[0:imageWidth+stepBytesDst/sizeof(float)*imageHeight]) create(pEuclDist[0:windowHeight*windowWidth], pNeighborhoodDiff[0:neighborHeight*neighborWidth])
+    #pragma acc data deviceptr(pSrcBorder[(windowHeight+2*(windowRadius+neighborRadius))*(windowWidth+2*(windowRadius+neighborRadius))], pSqrIntegralImage[(windowHeight+2*(windowRadius+neighborRadius)+1)*(windowWidth+2*(windowRadius+neighborRadius)+1)], pDst[imageHeight*imageWidth]) create(pEuclDist[0:windowHeight*windowWidth], pWindowIJCorr[windowHeight*windowWidth]) 
+    //#pragma acc data present(pSrcBorder[0:(imageWidth+windowRadius+neighborRadius)+stepBytesSrcBorder/sizeof(float)*(imageHeight+windowRadius+neighborRadius)],  pSqrIntegralImage[(windowHeight+2*(windowRadius+neighborRadius)+1)*(windowWidth+2*(windowRadius+neighborRadius)+1)], pDst[0:imageWidth+stepBytesDst/sizeof(float)*imageHeight]) create(pEuclDist[0:windowHeight*windowWidth], pWindowIJCorr[windowHeight*windowWidth])
     {
-        #pragma acc parallel vector_length(64)
+        #pragma acc parallel 
         {   
-    	    #pragma acc loop independent collapse(2)  private(pEuclDist[0:windowHeight*windowWidth])   
+    	    #pragma acc loop collapse(2)  private(pEuclDist[0:windowHeight*windowWidth], pWindowIJCorr[0:windowHeight*windowWidth])   
             for(int j = 0; j < imageHeight; j++)
     	    {
                
@@ -25,46 +25,63 @@ void DNLM_OpenACC(const float* pSrcBorder, int stepBytesSrcBorder, const float* 
                     const int indexPdstBase = j * (stepBytesDst/sizeof(float));
                     const int indexWindowStartBase = j * stepBytesSrcBorder/sizeof(float);
                     const int indexNeighborIJBase = (j + windowRadius) * stepBytesSrcBorder/sizeof(float);
+                    const int indexIINeighborIJBase = (j + windowRadius) * stepBytesSqrIntegral/sizeof(float);
+                    const int indexIINeighborIJBaseWOffset = (j + windowRadius + neighborWidth) * stepBytesSqrIntegral/sizeof(float);
+                    //Get sum area of neighborhood IJ
+                    const float sqrSumIJNeighborhood = pSqrIntegralImage[indexIINeighborIJBaseWOffset + (i + windowRadius + neighborWidth)]
+                                                    + pSqrIntegralImage[indexIINeighborIJBase + (i + windowRadius)] 
+                                                    - pSqrIntegralImage[indexIINeighborIJBase + (i + windowRadius + neighborWidth)]
+                                                    - pSqrIntegralImage[indexIINeighborIJBaseWOffset + (i + windowRadius)];
                     //Compute start pointer for each array
                     const float * restrict pWindowStart = (float*) &pSrcBorder[indexWindowStartBase + i];
                     const float * restrict pNeighborhoodStartIJ = (float*) &pSrcBorder[indexNeighborIJBase + i + windowRadius];
-                    //OLD const Npp32f *pUSMWindowStart = (Npp32f*) (pUSMImage + indexUSMWindowBase + i_w);
-                    //#pragma acc loop independent collapse(2)  private(pNeighborhoodStartIJ[0:neighborHeight*neighborWidth], pWindowStart[0:windowHeight*windowWidth]) 
-                    //#pragma acc loop seq
-                    #pragma acc loop independent  collapse(2) private(pNeighborhoodDiff[0:neighborHeight*neighborWidth])
+                    
+                    //Compute window correlation with IJ neighborhood
+                    #pragma acc loop collapse(2)
+                    for(int row_w = 0; row_w < windowHeight; row_w++)
+                    {
+                        for(int col_w = 0; col_w < windowWidth; col_w++)
+                        {
+                            float neighborCorrSum = 0;
+                            #pragma acc loop vector reduction(+:neighborCorrSum)
+                            for(int row_n = 0; row_n < neighborHeight; row_n++)
+                            {
+                                for(int col_n = 0; col_n < neighborWidth; col_n++)
+                                {
+                                    neighborCorrSum += pWindowStart[(col_w+col_n)+((row_w+row_n)*stepBytesSrcBorder/sizeof(float))] * pNeighborhoodStartIJ[col_n + (row_n * stepBytesSrcBorder/sizeof(float))]; 
+                                }
+                            }
+                        pWindowIJCorr[col_w + row_w * windowWidth] = neighborCorrSum;
+                        }
+                    }
+                   
+                    //#pragma acc loop
+                    #pragma acc loop  collapse(2) private(sqrSumIJNeighborhood, i, j)
                     for (int n = 0; n < windowHeight; n++)
                     {
 
                         for (int m = 0; m < windowWidth; m++)
                         {
                             //Compute base address for each array
-                            const int indexNeighborNMBase = n * (stepBytesSrcBorder/sizeof(float));
-                            const float * restrict pNeighborhoodStartNM = (float*) &pWindowStart[indexNeighborNMBase + m];
-
-                            float sumNeighborhoodDiff = 0;
-                            //loop reduce(+)
+                            const int indexIINeighborMNBase = (j + n) * stepBytesSqrIntegral/sizeof(float);
+                            const int indexIINeighborMNBaseWOffset = (j + n + neighborWidth) * stepBytesSqrIntegral/sizeof(float);
+                            
+                            const float sqrSumMNNeighborhood = pSqrIntegralImage[indexIINeighborMNBaseWOffset + (i + m  + neighborWidth)]
+                                                            + pSqrIntegralImage[indexIINeighborMNBase + (i + m )] 
+                                                            - pSqrIntegralImage[indexIINeighborMNBase + (i + m  + neighborWidth)]
+                                                            - pSqrIntegralImage[indexIINeighborMNBaseWOffset + (i + m )];
                             //#pragma acc loop seq
-                            #pragma acc loop  independent collapse(2) reduction(+:sumNeighborhoodDiff) 
-                            for(int row = 0; row < neighborHeight; row++)
-                            {
-                                for(int col = 0; col < neighborWidth; col++)
-                                {
-                                    pNeighborhoodDiff[row*neighborWidth + col] = pNeighborhoodStartNM[row*stepBytesSrcBorder/sizeof(float) + col] - pNeighborhoodStartIJ[row*stepBytesSrcBorder/sizeof(float) + col];
-                                    pNeighborhoodDiff[row*neighborWidth + col] = pow(pNeighborhoodDiff[row*neighborWidth + col], 2);
-                                    sumNeighborhoodDiff += pNeighborhoodDiff[row*neighborWidth + col];
-                                }
-                            }
-                        pEuclDist[n*windowWidth + m]= sqrt(sumNeighborhoodDiff);
+                            pEuclDist[n*windowWidth + m]= sqrSumIJNeighborhood + sqrSumMNNeighborhood -2*pWindowIJCorr[n*windowWidth + m];
                         }
                     }
 
                     float sumExpTerm = 0;
-                    #pragma acc loop independent collapse(2) reduction(+:sumExpTerm)
+                    #pragma acc loop collapse(2) reduction(+:sumExpTerm)
                     for(int row = 0; row < windowHeight; row++)
                     {
                         for(int col = 0; col < windowWidth; col++)
                         {
-                            pEuclDist[col + row * windowWidth] = pEuclDist[col + row * windowWidth] *  -1/2*(sigma_r * sigma_r);
+                            pEuclDist[col + row * windowWidth] = pEuclDist[col + row * windowWidth] *  -1/(2*(sigma_r * sigma_r));
                             pEuclDist[col + row * windowWidth] = expf(pEuclDist[col + row * windowWidth]);
                             sumExpTerm += pEuclDist[col + row * windowWidth];
                         }
@@ -72,7 +89,7 @@ void DNLM_OpenACC(const float* pSrcBorder, int stepBytesSrcBorder, const float* 
 
                     float filterResult = 0;            
                     //Reduce(+) 
-                    #pragma acc loop independent collapse(2) reduction(+:filterResult) 
+                    #pragma acc loop collapse(2) reduction(+:filterResult) 
                     for(int row = 0; row < windowHeight; row++)
                     {
                         for(int col = 0; col < windowWidth; col++)
